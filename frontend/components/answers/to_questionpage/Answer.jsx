fd617ce634efd0a84464b5aa499c58a4a85e2ce2
Try <khovct@gmail.com>
2020-11-18 15:52:25 -0800

    set initial hook value to object and allowed for more legible code

--- frontend/components/answers/to_questionpage/Answer.jsx
+++ frontend/components/answers/to_questionpage/Answer.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { fetchAnswerFromUser } from '../../../util/answer_util';
+import React from 'react';
 import CommentContainer from '../../comments/comment_container';
 import UserAnswerInfo from '../UserAnswerInfo';
 
@@ -15,45 +14,6 @@ function Answer(props) {
             {/* <CommentContainer answerId={answer.id} /> */}
         </li>
     )
-
-    return null;
 }
-
-
-// class AnswerIndexItem extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this._isMounted = $.ajax({
-//             url: `/api/answers/${this.props.answer.id}/users`,
-//             method: 'GET'
-//         });
-//         this.state = {
-//             usersThatCommented: {}
-//         }
-//     }
-
-//     componentDidMount() {
-//         this._isMounted.then(users => this.setState({usersThatCommented: users}))
-//     }
-
-//     componentWillUnmount() {
-//         this._isMounted.abort();
-//     }
-
-//     render() {
-//         const { answer, user } = this.props;
-//         if(user === undefined) {
-//             return null;
-//         }
-//         return (
-//             <li className="single-answer-container">
-//                 {/* <UserAnswerInfo answer={answer} user={user} /> */}
-//                 <p className="answer">{answer.text}</p>
-//                 {/* <CommentContainer answerId={answer.id} /> */}
-//             </li>
-//         )
-//     }
-// }
 
 export default Answer;
---
+++ frontend/components/questions/QuestionPage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import AnswersContainer from '../answers/to_questionpage/AnswersContainer';
+import { fetchQuestion } from '../../util/question_util';
+import AppContext from '../../contexts/AppContext';
+
+function QuestionPage(props) {
+    const { questionId } = props.match.params;
+    const [question, setQuestion] = useState({title: "", id: 0, user_id: ""});
+
+    useEffect(() => {
+        // replacing box shadow of nav bar
+        const navBar = document.getElementById("main-nav-bar")
+        navBar.style.boxShadow = "0 3px 2px -2px rgba(200,200,200,0.2)"
+        document.body.style.backgroundColor = "rgb(255, 255, 255)";
+        fetchQuestion(questionId)
+            .then(question => setQuestion(question));
+    }, [questionId])
+
+
+    return (
+        <section id="question-page-container">
+            <div className="question-answer-page">
+                <AppContext.Consumer>
+                    {currentUser => <AnswersContainer question={question} currentUser={currentUser}/>}
+                </AppContext.Consumer>
+                <div id="related-q-container">
+                    <div id="related-questions">
+                        <h6>Related Questions</h6>
+                        <div>
+                            <Link to="/question/7" className="related-question">Where and why did Try learn how to code?</Link>
+                            <Link to="/question/8" className="related-question">What are Try's experiences as a software engineer?</Link>
+                            <Link to="/question/9" className="related-question">Where can I find Try's portfolio?</Link>
+                            <Link to="/question/10" className="related-question">How can we best contact Try?</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>  
+    )
+}
+
+export default QuestionPage;
